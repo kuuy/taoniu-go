73b3c59740cc7b46b48a30fd482eed4faf7650ca
kuuy <master@kuuy.com>
2024-11-14 11:36:47 +0800

    fix

--- cryptos/commands/binance/futures/scalping.go
+++ cryptos/commands/binance/futures/scalping.go
@@ -4,6 +4,7 @@ import (
   "context"
   "errors"
   "fmt"
+  "github.com/shopspring/decimal"
   "log"
   "strconv"
   "time"
@@ -18,10 +19,11 @@ import (
 )
 
 type ScalpingHandler struct {
-  Db         *gorm.DB
-  Rdb        *redis.Client
-  Ctx        context.Context
-  Repository *repositories.ScalpingRepository
+  Db                *gorm.DB
+  Rdb               *redis.Client
+  Ctx               context.Context
+  Repository        *repositories.ScalpingRepository
+  SymbolsRepository *repositories.SymbolsRepository
 }
 
 func NewScalpingCommand() *cli.Command {
@@ -37,6 +39,11 @@ func NewScalpingCommand() *cli.Command {
       }
       h.Repository = &repositories.ScalpingRepository{
         Db: h.Db,
+      }
+      h.SymbolsRepository = &repositories.SymbolsRepository{
+        Db:  h.Db,
+        Rdb: h.Rdb,
+        Ctx: h.Ctx,
       }
       return nil
     },
@@ -81,6 +88,31 @@ func NewScalpingCommand() *cli.Command {
             return nil
           }
           if err := h.Flush(side); err != nil {
+            return cli.Exit(err.Error(), 1)
+          }
+          return nil
+        },
+      },
+      {
+        Name:  "check",
+        Usage: "",
+        Action: func(c *cli.Context) error {
+          side, _ := strconv.Atoi(c.Args().Get(0))
+          if side != 1 && side != 2 {
+            log.Fatal("side invalid")
+            return nil
+          }
+          if err := h.Check(side); err != nil {
+            return cli.Exit(err.Error(), 1)
+          }
+          return nil
+        },
+      },
+      {
+        Name:  "copy",
+        Usage: "",
+        Action: func(c *cli.Context) error {
+          if err := h.Copy(); err != nil {
             return cli.Exit(err.Error(), 1)
           }
           return nil
@@ -181,6 +213,40 @@ func (h *ScalpingHandler) Flush(side int) error {
     log.Println("scalping update", entity.Symbol, entity.Side, price)
 
     h.Db.Model(&entity).Update("price", price)
+  }
+  return nil
+}
+
+func (h *ScalpingHandler) Check(side int) error {
+  var scalping []*models.Scalping
+  h.Db.Model(&models.Scalping{}).Where("side=? AND status=1", side).Find(&scalping)
+  for _, entity := range scalping {
+    item, err := h.SymbolsRepository.Get(entity.Symbol)
+    if err != nil {
+      return err
+    }
+    _, stepSize, notional, err := h.SymbolsRepository.Filters(item.Filters)
+    if err != nil {
+      return nil
+    }
+    price, err := h.SymbolsRepository.Price(item.Symbol)
+    if err != nil {
+      return err
+    }
+    buyQuantity, _ := decimal.NewFromFloat(notional).Div(decimal.NewFromFloat(price)).Float64()
+    buyQuantity, _ = decimal.NewFromFloat(buyQuantity).Div(decimal.NewFromFloat(stepSize)).Ceil().Mul(decimal.NewFromFloat(stepSize)).Float64()
+    if buyQuantity*price >= 10.0 {
+      println("scalping", entity.Symbol, fmt.Sprintf("%v %v", notional, buyQuantity*price))
+    }
+  }
+  return nil
+}
+
+func (h *ScalpingHandler) Copy() error {
+  var scalping []*models.Scalping
+  h.Db.Model(&models.Scalping{}).Where("side=? AND status=1", 2).Find(&scalping)
+  for _, entity := range scalping {
+    h.Apply(entity.Symbol, 1)
   }
   return nil
 }
