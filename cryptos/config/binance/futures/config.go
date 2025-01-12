a9fc0c1479adbb768ba6a8b8cff0d15e8eeef978
kuuy <master@kuuy.com>
2025-01-12 12:54:49 +0800

    fix

--- cryptos/config/binance/futures/config.go
+++ cryptos/config/binance/futures/config.go
@@ -11,7 +11,7 @@ const (
   GAMBLING_SCALPING_MIN_AMOUNT                 = 16
   GAMBLING_SCALPING_MAX_AMOUNT                 = 440
   GAMBLING_SCALPING_PRICE_LOSE_PERCENT         = 20
-  GAMBLING_ANT_MIN_BINANCE                     = 3000
+  GAMBLING_ANT_MIN_BINANCE                     = 1200
   GAMBLING_ANT_MAX_AMOUNT                      = 125
   ASYNQ_QUEUE_TICKERS                          = "binance.futures.tickers"
   ASYNQ_QUEUE_KLINES                           = "binance.futures.klines"
